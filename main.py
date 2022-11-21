e9c64dab7240b3420c03d6b03955b275a476e3e0
m11107k09 <m11107k09@mail.ntust.edu.tw>
2022-11-21 23:02:37 +0800

    1121

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
+from sklearn.svm import SVC
+from itertools import product
+import multiprocessing as mp
 
 data = pd.read_csv("train.csv")
+score_list=[]
 
 def convert_wind(w):
     r=-1
@@ -24,58 +28,97 @@ def convert_wind(w):
     return angle
 
 
+def train_module(x,y,C,gamma,return_dict,procnum):
+    X_train, X_test, y_train, y_test = train_test_split(x,y,train_size=0.4)
+
+    grid=SVC(C=C,gamma=gamma,class_weight="balanced" )
+
+    #利用剛剛設定的參數來找到最適合的模型
+    grid.fit(X_train,y_train)
+
+    print(grid.score(X_test,y_test))
+    # score_list.append(grid.score(X_test,y_test))
+    return_dict[procnum]={'score':grid.score(X_test,y_test),'grid':grid}
+    # return_dict[procnum]['score'] = grid.score(X_test,y_test)
+    # return_dict[procnum]['grid']=grid
+
+
+
 #將資料分成訓練組及測試組
 from sklearn.model_selection import train_test_split
 
-data[["Attribute8","Attribute10"]]=data[["Attribute8","Attribute10"]].applymap(convert_wind)
+if __name__ == '__main__':
+    # freeze_support()
 
-data[["Attribute16","Attribute17"]]=data[["Attribute16","Attribute17"]].applymap(lambda x: 0 if x=="No" else 1)
+    data[["Attribute8","Attribute10"]]=data[["Attribute8","Attribute10"]].applymap(convert_wind)
 
-data=data.fillna(0)
+    data[["Attribute16","Attribute17"]]=data[["Attribute16","Attribute17"]].applymap(lambda x: 0 if x=="No" else 1)
 
-X = data[["Attribute2","Attribute3","Attribute4","Attribute5","Attribute6","Attribute7","Attribute8","Attribute9","Attribute10","Attribute11","Attribute12","Attribute13","Attribute14","Attribute15","Attribute16"]]
-y = data['Attribute17']
-X_train, X_test, y_train, y_test = train_test_split(X,y)
+    data=data.fillna(0)
+    train_para={}
 
-from sklearn.svm import SVC
+    X = data[["Attribute2","Attribute3","Attribute4","Attribute5","Attribute6","Attribute7","Attribute8","Attribute9","Attribute10","Attribute11","Attribute12","Attribute13","Attribute14","Attribute15","Attribute16"]]
+    y = data['Attribute17']
+    manager = mp.Manager()
+    
+    i=0
+    for C,gamma in product([1,10,100,1000],[1,0.1,0.01,0.0001]):
 
-#載入GridSearchCV
-from sklearn.model_selection import GridSearchCV
+        p_list = []
+        score_list=[]
+        train_para[i]={}
+        train_para[i]["accuracy"]=[]
+        return_dict = manager.dict()
+        
+        train_para[i]["C"]=C
+        train_para[i]["gamma"]=gamma
+        for j in range(5):
+            p=mp.Process(target=train_module,args=(X,y,C,gamma,return_dict,j))
+            p_list.append(p)
+            p.start()
 
-#GridSearchCV是建立一個dictionary來組合要測試的參數
-param_grid = {'C':[1,10,1000],'gamma':[1,0.01,0.0001]}
-# param_grid = {'C':[10],'gamma':[0.0001]}
+        for k in range(5):
+            p_list[k].join()    
 
-grid = GridSearchCV(SVC(),param_grid,n_jobs=5,verbose=3)
+        print(return_dict.values())
+        
+        print(pd.DataFrame(return_dict.values())['score'].mean())
+        train_para[i]["accuracy"]=pd.DataFrame(return_dict.values())['score'].mean()
+        train_para[i]['grid']=return_dict[0]["grid"]
+        i+=1
+        
+    print(train_para)
+    train_para=pd.DataFrame(train_para.values())
+    print(train_para)
 
-#利用剛剛設定的參數來找到最適合的模型
-grid.fit(X_train,y_train)
 
-print(grid.best_estimator_)
-# grid.best_estimator_=SVC(C=1, gamma=0.001).fit(X_train,y_train)
+    # train_para.mean()
 
-test_data=pd.read_csv("test.csv")
-test_data[["Attribute8","Attribute10"]]=test_data[["Attribute8","Attribute10"]].applymap(convert_wind)
-test_data[["Attribute16"]]=test_data[["Attribute16"]].applymap(lambda x: 0 if x=="No" else 1)
 
-#利用剛剛的最佳參考再重新預測測試組
-grid_predictions = grid.predict(X_test)
 
-from sklearn.metrics import classification_report, confusion_matrix
-#評估新參考的預測結果好壞
-print('confusion_matrix : ')
-print(confusion_matrix(y_test,grid_predictions))
-print('classification_report : ')
-print(classification_report(y_test,grid_predictions))
+    #利用剛剛的最佳參考再重新預測測試組
+    # grid_predictions = grid.predict(test_data[["Attribute2","Attribute3","Attribute4","Attribute5","Attribute6","Attribute7","Attribute8","Attribute9","Attribute10","Attribute11","Attribute12","Attribute13","Attribute14","Attribute15","Attribute16"]])
 
-#利用剛剛的最佳參考再重新預測測試組
-grid_predictions = grid.predict(test_data[["Attribute2","Attribute3","Attribute4","Attribute5","Attribute6","Attribute7","Attribute8","Attribute9","Attribute10","Attribute11","Attribute12","Attribute13","Attribute14","Attribute15","Attribute16"]])
 
-ans=list(pd.read_csv("ex_submit.csv")["ans"])
+    test_data=pd.read_csv("test.csv")
+    test_data[["Attribute8","Attribute10"]]=test_data[["Attribute8","Attribute10"]].applymap(convert_wind)
 
-from sklearn.metrics import classification_report, confusion_matrix
-#評估新參考的預測結果好壞
-print('confusion_matrix : ')
-print(confusion_matrix(ans,grid_predictions))
-print('classification_report : ')
-print(classification_report(ans,grid_predictions))
+    test_data[["Attribute16"]]=test_data[["Attribute16"]].applymap(lambda x: 0 if x=="No" else 1)
+
+    test_data=test_data.fillna(0)
+    a=0
+    ans=list(pd.read_csv("ex_submit.csv")["ans"])
+    input=test_data[["Attribute2","Attribute3","Attribute4","Attribute5","Attribute6","Attribute7","Attribute8","Attribute9","Attribute10","Attribute11","Attribute12","Attribute13","Attribute14","Attribute15","Attribute16"]]
+    for grid in train_para['grid']:
+        print(grid.score(input,ans))
+        # train_para['result'][a]=grid.score(input,ans)
+
+    
+
+
+    # from sklearn.metrics import classification_report, confusion_matrix
+    # #評估新參考的預測結果好壞
+    # print('confusion_matrix : ')
+    # print(confusion_matrix(ans,grid_predictions))
+    # print('classification_report : ')
+    # print(classification_report(ans,grid_predictions))
